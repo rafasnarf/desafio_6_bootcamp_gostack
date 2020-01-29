eefa23f31b4a65168fe89ecaf45d85af3335e9a2
rafasnarf <snarfelcv@gmail.com>
2020-01-29 00:12:10 -0300

    First Commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { StatusBar } from 'react-native';
+import 'react-native-gesture-handler';
+
+import './config/ReactotronConfig';
+import Routes from './routes';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle='light-content' backgroundColor='#7159c1' />
+      <Routes />
+    </>)
+}
