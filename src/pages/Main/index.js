90ac8ae8bf59261c2255cec7f2e459fe4aeeab25
rafasnarf <snarfelcv@gmail.com>
2020-02-04 23:10:19 -0300

    Elaboração do desafio

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Keyboard } from 'react-native';
+import { Keyboard, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
 import {
@@ -17,13 +19,43 @@ import {
 } from './styles';
 
 export default class Main extends Component {
+  static navigationOptions = {
+    title: 'Usuários',
+    headerTitleAlign: 'center',
+  }
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+  }
+
   state = {
     newUser: '',
     users: [],
+    loading: false,
   };
+
+  async componentDidMount() {
+    const users = await AsyncStorage.getItem('users');
+
+    if (users) {
+      this.setState({ users: JSON.parse(users) })
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { users } = this.state;
+
+    if (prevState.users !== users) {
+      AsyncStorage.setItem('users', JSON.stringify(users));
+    }
+  }
 
   handleAddUser = async () => {
     const { newUser, users } = this.state;
+
+    this.setState({ loading: true });
 
     const response = await api.get(`/users/${newUser}`);
 
@@ -35,14 +67,21 @@ export default class Main extends Component {
     }
     this.setState({
       users: [...users, data],
-      newUser: ''
+      newUser: '',
+      loading: false
     })
 
     Keyboard.dismiss();
   }
 
+  handleNavigate = (user) => {
+    const { navigation } = this.props;
+
+    navigation.navigate('User', { user });
+  }
+
   render() {
-    const { users, newUser } = this.state;
+    const { users, newUser, loading } = this.state;
     return (
       <Container>
         <Form>
@@ -55,8 +94,11 @@ export default class Main extends Component {
             returnKeyType='send'
             onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton onPress={this.handleAddUser}>
-            <Icon name='add' size={20} color='#fff' />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (<ActivityIndicator color="#fff" />
+            ) : (
+                <Icon name='add' size={20} color='#fff' />)}
+
           </SubmitButton>
         </Form>
         <List
@@ -67,7 +109,7 @@ export default class Main extends Component {
               <Avatar source={{ uri: item.avatar }} />
               <Name>{item.name}</Name>
               <Bio>{item.bio}</Bio>
-              <ProfileButton>
+              <ProfileButton onPress={() => this.handleNavigate(item)}>
                 <ProfileButtonText>Ver perfil</ProfileButtonText>
               </ProfileButton>
             </User>
@@ -79,7 +121,3 @@ export default class Main extends Component {
 
 }
 
-Main.navigationOptions = {
-  title: 'Usuários',
-  headerTitleAlign: 'center',
-}
